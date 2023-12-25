e9339d42de023144dd7367f425f5176d55f2ce36
Jim Greer <jbgreer@gmail.com>
2023-12-25 11:00:18 -0600

    remove pull_bootstrap.sh

--- pull_bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!env bash
-set -x
-
-# By default the generated configuration.nix is practically empty so we can overwrite it - feel free to review it first or move it
-curl -sSL https://raw.githubusercontent.com/jbgreer/Nixfiles/main/.config/nixos/systems/bootstrap.nix -o /mnt/etc/nixos/configuration.nix
