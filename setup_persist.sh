b67880646c63b944fde9538fc5072ac418459dcb
Jim Greer <jbgreer@gmail.com>
2023-12-26 10:46:06 -0600

    setup_persist recursive copy issue

--- setup_persist.sh
+++ setup_persist.sh
@@ -2,18 +2,25 @@
 set -x
 
 # Create directories to be mounted
-mkdir -p /persist/etc/{NetworkManager/system-connections,nixos,secureboot}
+mkdir -p /persist/etc/NetworkManager/system-connections
+mkdir -p /persist/etc/nixos
+mkdir -p /persist/etc/secureboot
 mkdir -p /persist/passwords
-mkdir -p /persist/var/lib/{bluetooth,colord,fprint,NetworkManager,power-profiles-daemon,systemd,upower}
+mkdir -p /persist/var/lib/bluetooth
+mkdir -p /persist/var/lib/colord
+mkdir -p /persist/var/lib/fprint
+mkdir -p /persist/var/lib/NetworkManager
+mkdir -p /persist/var/lib/power-profiles-daemon
+mkdir -p /persist/var/lib/systemd,upower
+mkdir -p /persist/var/lib/upower
 
 # Copy any existing configuration items
-cp -R {,/persist}/etc/NetworkManager/system-connections
-cp -R {,/persist}/etc/nixos
-cp -R {,/persist}/etc/secureboot
+cp -R /etc/NetworkManager/system-connections/* /persist/etc/NetworkManager/system-connections/
+cp -R /etc/nixos/* /persist/etc/nixos/
+cp -R /etc/secureboot/* /persist/etc/secureboot/
 
 # Move system ID and state files
-mv {,/persist}/etc/machine-id
-mv {,/persist}/var/lib/NetworkManager/secret_key
-mv {,/persist}/var/lib/NetworkManager/seen-bssids
-mv {,/persist}/var/lib/NetworkManager/timestamps
-#mv {,/persist}/var/lib/power-profiles-daemon/state.ini
+mv /etc/machine-id /persist/etc/machine-id
+mv /var/lib/NetworkManager/secret_key /persist/var/lib/NetworkManager/secret_key
+mv /var/lib/networkManager/seen-bssids /persist/var/lib/NetworkManager/seen-bssids
+mv /var/lib/NetworkMansager/timestampes /persist/var/lib/NetworkManager/timestamps
