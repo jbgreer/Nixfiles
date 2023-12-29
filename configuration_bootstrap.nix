527923ddbbf8692dce8d9e45e190ce17ecf0385d
Jim Greer <jbgreer@gmail.com>
2023-12-29 09:02:22 -0600

    trim configuration_bootstrap

--- configuration_bootstrap.nix
+++ configuration_bootstrap.nix
@@ -1,61 +1,51 @@
 # configuration_bootstrap.nix
-# Designed as first-boot configuration (bootstrap) for impermanence systems
-# Run nixos-generate-config --root=/mnt for hardware scan then overwrite with this as configuration.nix
-# Then you can reboot and get SecureBoot, TPM, /persist, and whatever else set up and use flakes to install
 { config, pkgs, lib, ... }: {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  # Bootloader settings
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
   boot.supportedFilesystems = [ "btrfs" "ntfs" "fat32" ];
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
   hardware.enableAllFirmware = true;
 
-  # Enable networking - set hostName
+  # TODO set hostname
   networking.hostName = "HOSTNAME";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
+  # TODO set locale and timezone
   i18n.defaultLocale = "en_US.utf8";
+  time.timeZone = "America/Chicago";
 
-  # Define a user account. Don't forget to change password
+  # TODO set username
   users.users.jbgreer = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      bind
       pfetch
-      wget
     ];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Install packages - vim & git are very useful
   environment.systemPackages = with pkgs; [
-    neovim
-    gcc
+    curl
     git
-    gnupg
+    vim
   ];
 
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
+  # set vim as default editor
+  programs.vim.enable = true;
+  programs.vim.defaultEditor = true;
 
-  #services.gnome.gnome-keyring.enable = lib.mkForce false;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
+  # WARNING! Be careful when changing.
   system.stateVersion = "23.11";
 
+  # Enable use of flakes
   nix.settings.experimental-features = "nix-command flakes";
 }
